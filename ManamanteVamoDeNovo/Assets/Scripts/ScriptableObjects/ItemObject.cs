eb6a3482f627ef65891ac0f171e9c0f2825ed7f9
Fabio Tiburcio <49411361+FabioTiburcio@users.noreply.github.com>
2020-08-26 16:01:32 -0300

    Atributos dos itens do inventario

--- ManamanteVamoDeNovo/Assets/Scripts/ScriptableObjects/ItemObject.cs
+++ ManamanteVamoDeNovo/Assets/Scripts/ScriptableObjects/ItemObject.cs
@@ -9,6 +9,14 @@ public enum ItemType
     Equipment,
     Crafting
 }
+
+public enum Attributes
+{
+    Agility,
+    Intelligence,
+    Stamina,
+    Strength
+}
 public class ItemObject : ScriptableObject
 {
     public int Id;
@@ -16,6 +24,13 @@ public class ItemObject : ScriptableObject
     public ItemType type;
     [TextArea(10,10)]
     public string description;
+    public ItemBuff[] buffs;
+
+    public Item CreateItem()
+    {
+        Item newItem = new Item(this);
+        return newItem;
+    }
 }
 
 [System.Serializable]
@@ -23,9 +38,38 @@ public class Item
 {
     public string name;
     public int Id;
+    public ItemBuff[] buffs;
     public Item(ItemObject item)
     {
         name = item.name;
         Id = item.Id;
+        buffs = new ItemBuff[item.buffs.Length];
+        for (int i = 0; i < buffs.Length; i++)
+        {
+            buffs[i] = new ItemBuff(item.buffs[i].min, item.buffs[i].max)
+            {
+                attribute = item.buffs[i].attribute
+            };
+            
+        }
+    }
+}
+
+[System.Serializable]
+public class ItemBuff
+{
+    public Attributes attribute;
+    public int value;
+    public int min;
+    public int max;
+    public ItemBuff(int _min, int _max)
+    {
+        min = _min;
+        max = _max;
+        GenerateValue();
+    }
+    public void GenerateValue()
+    {
+        value = UnityEngine.Random.Range(min, max);
     }
 }
