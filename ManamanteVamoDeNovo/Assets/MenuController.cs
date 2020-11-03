4e67b5b0725e93ab90dd727975dbf15e4c19ea18
juliomenes <juliomenes@users.noreply.github.com>
2020-11-02 21:30:08 -0300

    Menu de pausa, tutorial feito
    Tutorial está feito, porém falta substituir uma imagem ou outra, por exemplo a explicando skill secundária, pois queria mostrar com o shield de fogo porém ele não tem visual ainda. Menu de pausa também tá feito e funcionando e o menu inicial a mesma coisa, só faltam adicionar as configurações gerais e de áudio que não é tão difícil de fazer.

---
+++ ManamanteVamoDeNovo/Assets/MenuController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+
+public class MenuController : MonoBehaviour
+{
+	public void ChangeScene()
+	{
+		SceneManager.LoadScene(1);
+	}
+	public void Exit()
+	{
+	#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+	#else
+      Application.Quit();
+	#endif
+	}
+
+
+}
